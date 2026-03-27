e397f3366c06fced7c39f8520be5ab914f8f00a5
jordanhuaman <jordanelcracken@gmail.com>
2026-03-27 14:47:05 -0500

    feat: implement tab navigation layout and pages for home, planner, and insights

---
+++ src/app/(tabs)/insignts.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Insignts = () => {
+  return (
+    <View>
+      <Text>Hello insigntswewe 222</Text>
+    </View>
+  )
+}
+
+export default Insignts
---
+++ src/app/(tabs)/planner.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Planner = () => {
+  return (
+    <View>
+      <Text>
+        Planner
+      </Text>
+    </View>
+  )
+}
+
+export default Planner
