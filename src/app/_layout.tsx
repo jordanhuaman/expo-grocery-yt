e8d2f2642e95bf115b649261e01548dcd8f3c288
jordanhuaman <jordanelcracken@gmail.com>
2026-03-27 11:00:24 -0500

    feat: sign-in working

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,32 @@
+import { ClerkProvider } from '@clerk/expo';
+import { tokenCache } from '@clerk/expo/token-cache';
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { Stack } from 'expo-router';
 import React from 'react';
+import { useColorScheme } from 'react-native';
 import "../../global.css";
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+if (!publishableKey) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 
 export default function TabLayout() {
-  return <Stack/>
+
+  const colorSchema = useColorScheme();
+
+  console.log("colorSchema:", colorSchema);
+
+  const theme = colorSchema === "dark" || colorSchema === null ? DarkTheme : DefaultTheme;
+
+  return (
+    <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+      <ThemeProvider value={theme}>
+        <Stack screenOptions={{ headerShown: false }} />
+      </ThemeProvider>
+    </ClerkProvider>
+  )
 }
