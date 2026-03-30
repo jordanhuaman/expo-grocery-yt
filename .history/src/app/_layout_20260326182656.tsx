065b7ae85d6eea2cd6cdc6c12d4aaace74028a42
jordanhuaman <jordanelcracken@gmail.com>
2026-03-30 06:14:50 -0500

    chore: remove outdated history files and configurations

--- .history/src/app/_layout_20260326182656.tsx
+++
@@ -1,8 +0,0 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import React from 'react';
-import { useColorScheme } from 'react-native';
-
-
-export default function TabLayout() {
-  return 
-}
